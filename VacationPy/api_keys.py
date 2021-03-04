19c7e3c388c04875e20f668c0fa03bc61c0c972c
JA Gorman <jagorman42@gmail.com>
2021-03-03 19:04:33 -0600

    adding final files

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4ffda9f58a9e8cecac2ca28d8d7b9634"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDZ0NStSeomJLtlfN4UE3YMfDXWlBpi8A0"
